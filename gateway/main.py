c41108e7bf5df1bf5ae55cabb7fbe30756b3c163
Constantina Gilca <constantina.gilca@isa.utm.md>
2023-10-26 10:45:11 +0300

    Cache + gateway + heartbeat for scheduler done

--- gateway/main.py
+++ gateway/main.py
@@ -40,18 +40,6 @@ def add_new_service():
     for client in resp:
         REGISTERED_APPS[client] = resp[client]
 
-@app.route('/schedule', methods=['GET'])
-def get_schedule():
-    cache_data = get_data_from_cache(1,"GET /schedule")
-    if cache_data == None:
-        # send request to MS
-        data = json.dumps({"schedule":None})
-        log.info(f"Set to cache {data}")
-        save_data_in_cache(1,"GET /schedule",data)
-        return data
-    log.info("Got from cache")
-    return cache_data
-
 @app.route('/register', methods=['POST'])
 def register_client():
     if "Scheduler" not in REGISTERED_APPS.keys():
@@ -78,16 +66,66 @@ def login_client():
 
 @app.route('/schedule', methods=['POST'])
 def create_schedule():
-    log.info("HERE")
     if "Scheduler" not in REGISTERED_APPS.keys():
         return "Service not available"
     host = REGISTERED_APPS["Scheduler"]["host"]
     port = REGISTERED_APPS["Scheduler"]["port"]
     body = request.json
-    log.info("Sending request over")
     response = requests.post(url = f"http://{host}:{port}/api/schedule", json=body)
-    #log.info(schedule)
     return response.json()
+
+@app.route('/schedule', methods=['GET'])
+def get_schedule():
+    body = request.json
+    user_id = body["user_id"]
+    cache_data = get_data_from_cache(user_id,"GET /schedule")
+    if cache_data == None:
+        if "Scheduler" not in REGISTERED_APPS.keys():
+            return "Service not available"
+        host = REGISTERED_APPS["Scheduler"]["host"]
+        port = REGISTERED_APPS["Scheduler"]["port"]
+        response = requests.get(url = f"http://{host}:{port}/api/schedule?user_id={user_id}")
+        data = response.json()
+        data = {"schedule":response.json()}
+        save_data_in_cache(user_id,"GET /schedule",data)
+        return data
+    return cache_data
+
+@app.route('/schedule/day', methods=['GET'])
+def get_schedule_for_day():
+    body = request.json
+    user_id = body["user_id"]
+    day = body["day"]
+    cache_data = get_data_from_cache(user_id,f"GET /schedule/day_{day}")
+    if cache_data == None:
+        if "Scheduler" not in REGISTERED_APPS.keys():
+            return "Service not available"
+        host = REGISTERED_APPS["Scheduler"]["host"]
+        port = REGISTERED_APPS["Scheduler"]["port"]
+        response = requests.get(url = f"http://{host}:{port}/api/schedule/day?user_id={user_id}&day={day}")
+        data = response.json()
+        data = {"schedule":response.json()}
+        save_data_in_cache(user_id,f"GET /schedule/day_{day}",data)
+        return data
+    return cache_data
+
+@app.route('/schedule/today', methods=['GET'])
+def get_schedule_for_today():
+    body = request.json
+    user_id = body["user_id"]
+    cache_data = get_data_from_cache(user_id,f"GET /schedule/today")
+    if cache_data == None:
+        if "Scheduler" not in REGISTERED_APPS.keys():
+            return "Service not available"
+        host = REGISTERED_APPS["Scheduler"]["host"]
+        port = REGISTERED_APPS["Scheduler"]["port"]
+        body = request.json
+        response = requests.get(url = f"http://{host}:{port}/api/schedule/today?user_id={user_id}")
+        data = response.json()
+        data = {"schedule":response.json()}
+        save_data_in_cache(user_id,f"GET /schedule/today",data)
+        return data
+    return cache_data
 
 if __name__ == "__main__":
     register()
